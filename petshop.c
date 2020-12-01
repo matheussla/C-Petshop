8358015a5126b3648a2661eba0004d202c84ed9f
ma89020 <matheusabreu_southsystem@getnet.com.br>
2020-11-30 22:49:37 -0300

    add stock and prints

--- petshop.c
+++ petshop.c
@@ -31,9 +31,18 @@ typedef struct Service{
     int registro;
 } Service;
 
+typedef struct Stock{
+    double Racao;
+    double Shampoo;
+    double Brinquedo;
+    double ;
+} Stock;
+
 Client* createClient(int size);
 
 Animal* createAnimal(int size);
+
+Stock* createStock(int size);
 
 void clear_buffer(void);
 
@@ -47,12 +56,14 @@ int main(int argc, char* argv[]){
     Client* client;
     Animal* animal;
     Service* service;
+    Stock* stock;
     char selection = '0';
     Animal* animalSearch;
     
     client = createClient(LIMITE);
     animal = createAnimal(LIMITE);
     service = createService(PRECO);
+    stock = createStock(PRECO);
 
     do {
         printf("[1] - Cadastrar Cliente\n");
@@ -60,7 +71,9 @@ int main(int argc, char* argv[]){
         printf("[3] - Cadastrar Servico\n");
         printf("[4] - Cadastrar Estoque\n");
         printf("[5] - Buscar Animal\n");
-        printf("[6] - Encerrar Programa\n\n\n");
+        printf("[6] - Imprimir Estoque\n\n\n");
+        printf("[7] - Imprimir Servicos\n\n\n");
+        printf("[8] - Encerrar Programa\n\n\n");
         
         printf("Sua escolha? ");
         selection = getchar();
@@ -76,32 +89,38 @@ int main(int argc, char* argv[]){
                 accessService(service, PRECO);
                 break;
             case '4':
-                printf("------------Estoque-------------");
+                 accessStock(stock, PRECO);
                 break;        
             case '5':
                 if ((animalSearch = search(animal, LIMITE)) != NULL) {
                     printf("Nome: %s\n", animalSearch->nome);
                     printf("Idade: %d\n", animalSearch->idade);
                     printf("Cor: %s\n", animalSearch->cor);
-                    printf("Raça: %s\n", animalSearch->raca);
+                    printf("Raca: %s\n", animalSearch->raca);
                     printf("Registro: %d\n", animalSearch->registro);
                     
                     if (animalSearch->vacinado == true) {
                         printf("Vacinado: sim\n");
                     }else{
-                        printf("Vacinado: não\n");
+                        printf("Vacinado: nao\n");
                     }
                 }else{
-                    printf("Animal não cadastrado.\n");
+                    printf("Animal nao cadastrado.\n");
                 }
                 break;
             case '6':
+                 printStock(stock);
+                break;      
+             case '7':
+                 printService(service);
+                break;          
+            case '8':
                 printf("Encerrando o programa.");
                 break;
             default:
-                printf("Opção inválida.Tente novamente.\n");
+                printf("Opcao Invalida. Tente novamente.\n");
         }
-    } while (selection != '6');
+    } while (selection != '8');
     
     return 0;
 }
@@ -196,7 +215,7 @@ void accessAnimal(Animal* record, int size){
         printf("Cor: ");
         fgets(record[count].cor, 15, stdin);
         
-        printf("Raça: ");
+        printf("Raca: ");
         fgets(record[count].raca, 15, stdin);
         
         record[count].registro = rg++;
@@ -219,7 +238,7 @@ void accessService(Service* record, int size){
     do {
         system("clear");
         
-        printf("Cadastro Preço do Servico\n\n");
+        printf("Cadastro Pretacao de Servico\n\n");
         
         printf("Banho: ");
         fgets("%ld", &record[count].banho);
@@ -229,6 +248,39 @@ void accessService(Service* record, int size){
         
         printf("Vacina: ");
         fgets("%ld", &record[count].vacina);
+
+        clear_buffer();
+        
+        record[count].registro = rg++;
+        
+        count++;
+    
+        printf("\n\n\nModificar o preco? ");
+        
+        keep = getchar();
+        
+    } while (count < size && (keep != 'n' && keep != 'N'));
+
+}
+
+void accessStock(Stock* record, int size){
+    int count = 0;
+    static int rg;
+    char keep;
+    
+    do {
+        system("clear");
+        
+        printf("Cadastro Estoque\n\n");
+        
+        printf("Racao: ");
+        fgets("%ld", &record[count].racao);
+        
+        printf("Shampoo: ");
+        fgets("%ld", &(record[count].shampoo));
+        
+        printf("Brinquedo: ");
+        fgets("%ld", &record[count].brinquedo);
 
         clear_buffer();
         
@@ -263,4 +315,28 @@ Animal* search(Animal* record, int lim){
     }
     
     return response;
+}
+
+int printStock(record){
+    int count = 0;
+    
+    FILE*arq = fopen("stock.txt", "w");
+    
+    fprint(arq, record);
+    
+    fclose(arq);
+    
+    return 0;
+}
+
+int printService(record){
+    int count = 0;
+    
+    FILE*arq = fopen("service.txt", "w");
+    
+    fprint(arq, record);
+    
+    fclose(arq);
+    
+    return 0;
 }
